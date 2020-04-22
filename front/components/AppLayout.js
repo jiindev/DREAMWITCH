c46f24225794eaee3c7078851801d63a047fb178
jiindev <jiindev@gmail.com>
2020-04-22 14:46:34 +0900

    AppLayout test

---
+++ front/components/AppLayout.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect, useCallback } from "react";
+import Link from "next/link";
+// import Head from 'next/head';
+// import AppLaout from '../components/AppLayout';
+import propTypes from 'prop-types';
+import Character from "./Character";
+import History from "./HistoryList";
+import TodoList from "./TodoList";
+import Shop from "./Shop";
+import { useSelector, useDispatch } from "react-redux";
+import Router from "next/router";
+import { LOAD_USER_REQUEST, LOG_OUT_REQUEST } from "../reducers/user";
+import styled from 'styled-components';
+import { LOAD_HISTORIES_REQUEST } from "../reducers/history";
+import { LOAD_TODOS_REQUEST } from "../reducers/todo";
+import { LOAD_ITEMS_REQUEST } from "../reducers/item";
+import HistoryList from "./HistoryList";
+
+const AppLayout = ({children}) => {
+  const dispatch = useDispatch();
+  const { me, logInErrorReason } = useSelector((state) => state.user);
+
+  useEffect(()=>{
+    if(!me){
+      Router.push('/login');
+    }
+  }, [me]);
+
+
+  const onLogout = () => {
+    dispatch({
+      type: LOG_OUT_REQUEST
+    })
+  }
+
+  return (
+    <>
+      <Wrap>
+       <TopContent>
+         <UserStatue>
+            <span>{me && me.star}별</span><span>{me && me.level}레벨</span>
+            <button onClick={onLogout}>로그아웃</button>
+         </UserStatue>
+          <Character></Character>
+        <Tab>
+          <ul>
+            <Link href="/test"><TabItem><TabIcon iconName={'star'}/></TabItem></Link>
+            <Link href="/history"><TabItem><TabIcon iconName={'list'}/></TabItem></Link>
+            <Link href="/item"><TabItem><TabIcon iconName={'shop'}/></TabItem></Link>
+          </ul>
+        </Tab>
+        </TopContent>
+        <Page>
+          {children}
+        </Page>
+        </Wrap>
+    </>
+  );
+  
+};
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+
+const Page = styled.div`
+  background-color: ${props => props.theme.yellowLight};
+  width: 100%;
+  padding: 38px 15px 0 15px;
+  box-sizing: border-box;
+  flex: 1;
+  overflow: scroll;
+  position: relative;
+`;
+
+const Tab = styled.nav`
+  & ul {
+    display: flex;
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+  }
+`;
+
+const TabItem = styled.li`
+    background-color: ${props => (props.active ? props.theme.yellowLight : props.theme.purpleLight)};
+    color: ${props => props.theme.purpleDark};
+    padding: 12px 0;
+    flex: 0.33333;
+    border-radius: 20px 20px 0 0;
+    text-align: center;
+    transition: all .2s ease;
+    cursor: pointer;
+`;
+
+const TabIcon = styled.i`
+  width: 16px;
+  height: 16px;
+  background: ${props => `url('/static/icons/tabbutton_${props.iconName}.svg')`};
+  display: inline-block;
+`;
+
+const TopContent = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+`;
+
+const UserStatue = styled.div`
+  position: absolute;
+  top: 0;
+  z-index: 99;
+`;
+
+AppLayout.propTypes = {
+    children: propTypes.node
+}
+
+
+export default AppLayout;
---
+++ front/pages/test.js
@@ -0,0 +1,98 @@
+import React, { useState, useRef, createRef, useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import TodoItem from "../components/TodoItem";
+import AddTodo from '../components/AddTodo';
+import TodoStatue from '../components/TodoStatue';
+import AppLayout from "../components/AppLayout";
+import { H2, Date, Button } from '../components/styledComponents/PageComponent';
+import styled from 'styled-components';
+import { LOAD_USER_REQUEST } from "../reducers/user";
+import { LOAD_TODOS_REQUEST } from "../reducers/todo";
+
+const TodoList = () => {
+  const dispatch = useDispatch();
+  const { todos, date, isCleared } = useSelector((state) => state.todo);
+  const [started, setStarted] = useState(false);
+
+  
+  const onStartTodo = useCallback(() => {
+    setStarted(true);
+  }, []);
+
+
+  return (
+    <>
+      <AppLayout>
+      {todos && todos[0] || started ? (
+        <TodoPage>
+          <div>
+            <H2>할 일 목록 <Date>{date}</Date></H2>
+            
+          </div>
+          <TodoUl>
+            {isCleared ? 
+              todos.map((c, i)=> {
+                return <li key={i}>{c.content}</li>
+              })
+            :
+            <>
+              {todos.map((c, i) => {
+                return <TodoItem todo={c}/>;
+              })}
+              <AddTodo/>
+            </>
+            }
+          </TodoUl>
+          <TodoStatue/>
+        </TodoPage>
+      ) : (
+        <StartTodo>
+          <div>
+            <H2>오늘도 꿈을 향해! 할 일 목록 적어볼까?</H2>
+          </div>
+          <StartIllust>지수 일러스트</StartIllust>
+          <Button onClick={onStartTodo}>시작하기</Button>
+        </StartTodo>
+      )}
+      </AppLayout>
+    </>
+  );
+};
+
+const TodoPage = styled.div`
+  width: 100%;
+`;
+
+const TodoUl = styled.ul`
+  padding-bottom: 100px;
+`;
+
+const StartIllust = styled.span`
+  width: 100%;
+  height: 300px;
+  display: flex;
+  flex: 1;
+  display: inline-block;
+  border: 1px solid black;
+  border-radius: 20px;
+  text-align: center;
+`;
+
+const StartTodo = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  position: relative;
+  width: 100%;
+`;
+
+TodoList.getInitialProps = async (context) => {
+    context.store.dispatch({
+      type: LOAD_USER_REQUEST
+    })
+    context.store.dispatch({
+      type: LOAD_TODOS_REQUEST
+    })
+  }
+
+export default TodoList;
