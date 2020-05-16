da4937b131222360914d414359a8f85529c27b4a
jiindev <jiindev@gmail.com>
2020-05-17 00:26:37 +0900

    배포 시 주소설정 변경

---
+++ front/config/config.js
@@ -0,0 +1,2 @@
+const backUrl = process.env.NODE_ENV == 'production' ? 'http://api.dreamwitch.kr' : 'http://localhost:3065';
+export {backUrl}
--- front/sagas/index.js
+++ front/sagas/index.js
@@ -4,7 +4,9 @@ import user from "./user";
 import history from './history';
 import item from './item';
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3065/api";
+import { backUrl } from "../config/config";
+
+axios.defaults.baseURL = `${backUrl}/api`;
 
 export default function* rootSaga() {
   yield all([fork(todo), fork(user), fork(history), fork(item)]);
