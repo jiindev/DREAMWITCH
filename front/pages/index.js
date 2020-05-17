90f52750b38bbaf7777ffab05641f05894db27e2
jiindev <jiindev@gmail.com>
2020-05-17 11:40:25 +0900

    wrap 100% 변경 테스트

--- front/pages/index.js
+++ front/pages/index.js
@@ -132,10 +132,7 @@ export const Loading = styled.span`
 export const Wrap = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
-  @media only screen and (min-width: 769px) {
-    height: 100%;
-  }
+  height: 100%;
 `;
 
 export const Tab = styled.nav`
